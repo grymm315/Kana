145664e0da1ba3f7a82dfbdd54598cbff95bb1c9
Chris P <chris.phillips08@gmail.com>
2015-10-29 14:07:31 -0700

    Initial Commit

---
+++ KanaTests/KanaTests.swift
@@ -0,0 +1,36 @@
+//
+//  KanaTests.swift
+//  KanaTests
+//
+//  Created by Chris Phillips on 10/29/15.
+//  Copyright © 2015 Mad Men of Super Science, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Kana
+
+class KanaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
